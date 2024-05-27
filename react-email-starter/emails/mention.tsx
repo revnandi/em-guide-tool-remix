aa136ea10ef5ddd2745700dbc4cdb904c711ecba
revnandi <revnandi@gmail.com>
2024-05-28 00:51:22 +0200

    add react-email

---
+++ react-email-starter/emails/mention.tsx
@@ -0,0 +1,102 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
+
+export const MentionNotificationEmail = () => (
+  <Html>
+    <Head />
+    <Preview>You're now ready to make live transactions with Stripe!</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Section style={box}>
+          <Img
+            src={`${baseUrl}/static/stripe-logo.png`}
+            width="49"
+            height="21"
+            alt="Stripe"
+          />
+          <Hr style={hr} />
+          <Text style={paragraph}>
+            [Test Name] just mentioned you in a comment on EM GUIDE.
+          </Text>
+          <Text style={paragraph}>
+            To view the full comment and respond, follow this link:
+          </Text>
+          <Button style={button} href="https://dashboard.stripe.com/login">
+            View the comment
+          </Button>
+        </Section>
+      </Container>
+    </Body>
+  </Html>
+);
+
+export default MentionNotificationEmail;
+
+const main = {
+  backgroundColor: "#f6f9fc",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
+};
+
+const container = {
+  backgroundColor: "#ffffff",
+  margin: "0 auto",
+  padding: "20px 0 48px",
+  marginBottom: "64px",
+};
+
+const box = {
+  padding: "0 48px",
+};
+
+const hr = {
+  borderColor: "#e6ebf1",
+  margin: "20px 0",
+};
+
+const paragraph = {
+  color: "#525f7f",
+
+  fontSize: "16px",
+  lineHeight: "24px",
+  textAlign: "left" as const,
+};
+
+const anchor = {
+  color: "#556cd6",
+};
+
+const button = {
+  backgroundColor: "#656ee8",
+  borderRadius: "5px",
+  color: "#fff",
+  fontSize: "16px",
+  fontWeight: "bold",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  width: "100%",
+  padding: "10px",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
+  lineHeight: "16px",
+};
